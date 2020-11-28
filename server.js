5402475292448b3d22b67cf2c4a3a3f9bc832e60
Mubashir Hasan <mubashirhasan716@gmail.com>
2020-11-28 21:53:25 +0530

    ran into trouble. need to fix it

--- server.js
+++ server.js
@@ -5,40 +5,10 @@ const ShortUrl = require("./models/shortUrl");
 const app = express();
 const bcrypt = require("bcrypt");
 const Auth = require("./models/users");
-const passport = require("passport");
-const flash = require("express-flash");
-const session = require("express-session");
-
-mongoose.connect(
-  `mongodb://Mubashir:y4gQEVGPQKq0gQ9c@cluster0-shard-00-00.ochei.mongodb.net:27017,cluster0-shard-00-01.ochei.mongodb.net:27017,cluster0-shard-00-02.ochei.mongodb.net:27017/shortnner?ssl=true&replicaSet=atlas-5s5ecj-shard-0&authSource=admin&retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
-const initializePassport = require("./passport-config");
-initializePassport(
-  passport,
-  (email) => {
-    return Auth.find((user) => user.email === email);
-  },
-  (id) => Auth.find((user) => user.id === id)
-);
+const { name } = require("ejs");
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
-app.use(flash);
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
-app.use(passport.initialize);
-app.use(passport.session);
 
 app.get("/s", async (req, res) => {
   const shortUrls = await ShortUrl.find();
@@ -49,21 +19,37 @@ app.get("/in", async (req, res) => {
     "<div><h1> Log in to use Short Shortnner </h1><h4><a href='https://short-shortnner.vercel.app/'>Go back to Short Shortnner</a></h4><h4><a href='https://shortnner.herokuapp.com/'>Start Shortening your URLs</a></h4></div>"
   );
 });
+app.set("views", __dirname + "/views");
+app.engine("html", require("ejs").renderFile);
 
 app.get("/", (req, res) => {
-  res.render("app");
+  res.render("app.html");
 });
+app.set("views", __dirname + "/views");
+app.engine("html", require("ejs").renderFile);
 
 app.get("/login", (req, res) => {
-  res.render("login.html", { name: req.body.name });
+  res.render("login.html");
 });
-app.post("/login", () => {
-  passport.authenticate("local"),
-    {
-      successRedirect: "/s",
-      failureRedirect: "/login",
-      failureFlash: true,
-    };
+app.post("/login", async (req, res) => {
+  const user = await Auth.findOne({
+    name: req.body.name,
+    password: req.body.password,
+    email: req.body.email,
+  });
+
+  if (user === null) {
+    return res.status(400).send("No such User exists");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("Successfully logged in.");
+    } else {
+      res.send("Incorrect User Information. Try again.");
+    }
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
 app.get("/register", (req, res) => {
@@ -72,16 +58,26 @@ app.get("/register", (req, res) => {
 
 app.post("/register", async (req, res) => {
   try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    mongoose.connect(
+      `mongodb+srv://Mubashir:y4gQEVGPQKq0gQ9c@cluster0.ochei.mongodb.net/user:${Math.floor(
+        Math.random() * 100000
+      )}?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    const hashedPassword = await bcrypt.hash(req.body.password);
+
     await Auth.create({
       password: hashedPassword,
       email: req.body.email,
       name: req.body.name,
     });
     res.redirect("/login");
+    res.status(200);
   } catch {
-    alert("Registration Failed.");
-    res.redirect("/register");
+    res.send("Registration Failed.");
   }
 }); //works now
 app.post("/shortUrls", async (req, res) => {
@@ -102,4 +98,4 @@ app.get("/:shortUrl", async (req, res) => {
   res.redirect(shortUrl.full);
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(3000, () => console.log("running at 3000 port"));
