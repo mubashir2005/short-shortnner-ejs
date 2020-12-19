7a4f2c22b1e73c099b12716241d8622e7358e8d3
Mubashir Hasan <mubashirhasan716@gmail.com>
2020-12-19 19:27:08 +0530

    authentication now works

--- server.js
+++ server.js
@@ -32,56 +32,6 @@ app.get("/auth", (req, res) => {
     res.render("auth.html");
 });
 
-//passport js
-
-//google auth
-var passport = require("passport");
-var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
-
-// Use the GoogleStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and Google
-//   profile), and invoke a callback with a user object.
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID:
-        "220565751325-tort2ob8nqj99cohi8al4st2ud217ml3.apps.googleusercontent.com",
-      clientSecret: "GfSDog4jKY7r-Z59H1EgvlBq",
-      callbackURL: "http://localhost:3000/auth/google/callback",
-    },
-    function (accessToken, refreshToken, profile, done) {
-      User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        return done(err, user);
-      });
-    }
-  )
-);
-
-// GET /auth/google
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in Google authentication will involve
-//   redirecting the user to google.com.  After authorization, Google
-//   will redirect the user back to this application at /auth/google/callback
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["https://www.googleapis.com/auth/plus.login"],
-  })
-);
-
-// GET /auth/google/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
-app.get(
-  "/auth/google/callback",
-  passport.authenticate("google", { failureRedirect: "/login" }),
-  function (req, res) {
-    res.redirect("/");
-  }
-);
 
 app.post("/shortUrls", async (req, res) => {
   await ShortUrl.create({ full: req.body.fullUrl });
@@ -101,4 +51,4 @@ app.get("/:shortUrl", async (req, res) => {
   res.redirect(shortUrl.full);
 });
 
-app.listen(process.env.PORT || 3000, () => console.log("running at 300 port"));
+app.listen(process.env.PORT || 3000, () => console.log("running at 3000 port"));
